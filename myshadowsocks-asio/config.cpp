2391be1d6b1549945a019da2f19f4cf3c4c22b29
frank <could.net@gmail.com>
2018-05-30 23:56:49 +0800

    config.cpp

---
+++ myshadowsocks-asio/config.cpp
@@ -0,0 +1,55 @@
+/*config.cpp
+				frank May 30, 2018
+*/
+#include <iostream>
+#include <regex>
+#include <fstream>
+#include <string>
+#include "config.h"
+
+using namespace std;
+
+bool SSConfig::ReadConfig(std::string file_name)
+{
+	ifstream f(file_name);
+	if(! f)
+	{
+		std::cerr<<"failed to open file: "<<file_name<<"\n";
+		return false;
+	}
+
+	// read aaa = bbb
+	std::regex reg("^([\\w_]+)[ ]?=[ ]?([\\w\\d\\.]+)$");
+
+	std::string line;
+	while(std::getline(f, line))
+	{
+		std::smatch match;
+		if(std::regex_match(line, match, reg))
+		{
+			//cout<<match[1].str()<<" = "<<match[2].str()<<"\n";
+			string name = match[1].str();
+			string value = match[2].str();
+
+			if(name == "server_ip")
+			{
+				server_ip = value;
+			}
+			else if(name == "server_port")
+			{
+				server_port = std::stoi(value);
+			}
+			else if(name == "local_port")
+			{
+				local_port = std::stoi(value);
+			}
+			else
+			{
+				std::cerr<<"invalid config line: "<<line<<"\n";
+				return false;
+			}
+		}
+	}
+
+	return true;
+}

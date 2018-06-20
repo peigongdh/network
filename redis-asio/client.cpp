87cd0e0347ba2e71119779610d280973bf70acbc
frank <could.net@gmail.com>
2018-06-20 17:24:03 +0800

    added client

---
+++ redis-asio/client.cpp
@@ -0,0 +1,8 @@
+/*
+client.cpp
+                      frank Jun 20, 2018
+*/
+
+
+// asio async read from console, see :
+// https://www.boost.org/doc/libs/1_67_0/doc/html/boost_asio/example/cpp03/chat/posix_chat_client.cpp

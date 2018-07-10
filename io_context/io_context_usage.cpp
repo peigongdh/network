081351bd4b82fbab184cfd876517a49dbb7bc913
frank <could.net@gmail.com>
2018-07-10 16:29:05 +0800

    know what's up with run and poll, dispatch and post

--- io_context/io_context_usage.cpp
+++ io_context/io_context_usage.cpp
@@ -34,41 +34,41 @@ void MultiThreadRun()
 	asio::io_context io; // 无论是post还是dispatch，其待调用的函数都是在io_context::run的线程里面运行
 	asio::strand<asio::io_context::executor_type> str(io.get_executor()); // strand的用法，这里用来防止cout输出错乱
 
+	thread t1([&io, &str]() {
+		//asio::executor_work_guard<asio::io_context::executor_type> wg
+		auto wg = asio::make_work_guard(io) ; // executor_work_guard可以防止io.run()在事件处理完以后退出
+		io.run(); // 从任何线程发起的post/dispatch的函数都在调用run/poll的线程中执行
+	});
+
 	thread t2([&io, &str]() {
+//		cout<<"thread2 id:"<<std::this_thread::get_id()<<"\n";
+
 		for(int i = 0; i < 10; ++i)
 		{
-			// 如果是调用dispatch的线程和调用io_context::run的线程不是同一个线程，则dispatch和post的效果是一样的。
+			// 如果是调用dispatch的线程和调用io_context::run的所有线程都不是同一个线程(因为可以在多个线程中调用run)，则dispatch和post的效果是一样的。
 			// 此处就是一样, dispatch就等于post，都会被post到另一个线程中去执行
-//			io.post([&io, &str]() {
+			io.dispatch(asio::bind_executor(str, []() {
+				cout<<"dispatch, threadid:"<<std::this_thread::get_id()<<"\n";
+			}));
+			io.post(asio::bind_executor(str, []() {
+				cout<<"post, threadid:"<<std::this_thread::get_id()<<"\n";
+			}));
+
+			//下面就不一样了，首先是回调函数被post到t1中去执行
+			//后来再执行内部的dispatch和post时，已经是在t1中了，所以会立即执行dispatch，而post会最后执行
+			//得到的效果就是所有的disatch都执行完了，才执行post
+			io.post([&io, &str]() {
 				io.dispatch(asio::bind_executor(str, []() {
-					cout<<"dispatch, threadid:"<<std::this_thread::get_id()<<"\n";
+					cout<<"dispatch will run first, threadid:"<<std::this_thread::get_id()<<"\n";
 				}));
 				io.post(asio::bind_executor(str, []() {
-					cout<<"post, threadid:"<<std::this_thread::get_id()<<"\n";
+					cout<<"post will run after all dispatch, threadid:"<<std::this_thread::get_id()<<"\n";
 				}));
-//			});
+			});
 		}
-
-//		cout<<"thread2 id:"<<std::this_thread::get_id()<<"\n";
 	});
 
 	t2.join();
-
-	thread t1([&io, &str]() {
-		int times = 0;
-		while(++times < 10)
-		{
-			io.restart();
-			io.run(); // 注意run调用的时机，如果run最先启动，起初没有任何事件，run会退出，后面post进去的回调都不会执行。
-			for(int i = 0; i < 10; ++i)
-			{
-				io.post(asio::bind_executor(str, []() { cout<<"====>post, this one is after next dispatch\n"; } ));
-				io.dispatch(asio::bind_executor(str, []() { cout<<"====>dispatch, this one will run first\n"; }));
-			}
-		}
-	});
-
-	//t2.join();
 	t1.join();
 
 }

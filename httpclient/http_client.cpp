181a19b5f3d25e5c60252c38cacf37be62b71032
zhangpei <zhangpei@ezbuy.com>
2018-10-18 17:09:47 +0800

    format code

--- httpclient/http_client.cpp
+++ httpclient/http_client.cpp
@@ -11,22 +11,19 @@ http_client_test.cpp
 
 using namespace std;
 
-void OnFinish(std::shared_ptr<AsyncHttpClient> client)
-{
-	std::cout<<"call back in finish, err: "<<client->Err()<<"\n";
+void OnFinish(const std::shared_ptr<AsyncHttpClient> &client) {
+    std::cout << "call back in finish, err: " << client->Err() << "\n";
 }
 
-int main()
-{
-	std::map<string, std::shared_ptr<AsyncHttpClient>> urls = {
-			{"http://www.qq.com", nullptr},
-			{"http://en.cppreference.com/w/cpp/regex/match_results", nullptr},
-			};
+int main() {
+    std::map<string, std::shared_ptr<AsyncHttpClient>> urls = {
+            {"http://www.qq.com",                                    nullptr},
+            {"http://en.cppreference.com/w/cpp/regex/match_results", nullptr},
+    };
     asio::io_context io_context;
-    for(auto&& it : urls)
-    {
-    		it.second = std::make_shared<AsyncHttpClient>(io_context, it.first);
-    		it.second->Start(OnFinish);
+    for (auto &&it : urls) {
+        it.second = std::make_shared<AsyncHttpClient>(io_context, it.first);
+        it.second->Start(OnFinish);
     }
 
     io_context.run();
